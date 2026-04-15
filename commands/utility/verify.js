ea3f9f7f42771c06ace2d27932f73220f2808b39
Neo <neo@pulumi.com>
2026-04-15 23:38:10 +0000

    update verify embed text and button

--- commands/utility/verify.js
+++ commands/utility/verify.js
@@ -15,17 +15,17 @@ module.exports = {
       .setDescription([
         'Please verify using the button below.\u{1F447}',
         '',
-        'We are trusted and can guarantee by verifying you are safe',
-        'We do this verification to ensure we can bring you back if our server gets deleted',
-        'We have over 6000+ vouches so you are in safe hands',
-      ].join('\n'))
-      .setTimestamp();
+        '* We are trusted and can guarantee by verifying you are safe',
+        '* We do this verification to ensure we can bring you back if our server gets deleted',
+        '* We have over 6000+ vouches so you are in safe hands',
+      ].join('\n'));
 
     const row = new ActionRowBuilder().addComponents(
       new ButtonBuilder()
         .setLabel('Authorize')
         .setStyle(ButtonStyle.Link)
-        .setURL(VERIFY_URL),
+        .setURL(VERIFY_URL)
+        .setEmoji('\u2705'),
     );
 
     message.channel.send({ embeds: [embed], components: [row] });
