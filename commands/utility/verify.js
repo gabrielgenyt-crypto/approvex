3972ce3c5a11486fcb61ae9f77205ee37517a522
Neo <neo@pulumi.com>
2026-04-15 23:46:46 +0000

    update verify text, remove tick emoji from button

--- commands/utility/verify.js
+++ commands/utility/verify.js
@@ -12,14 +12,13 @@ module.exports = {
 
     const embed = new EmbedBuilder()
       .setColor(0x57f287)
-      .setDescription('Please **verify** using the button below.\u{1F447}\n\u2022 We are trusted and can guarantee by verifying you are safe\n\u2022 We do this verification to ensure we can bring you back if our server gets deleted\n\u2022 We have over 6000+ vouches so you are in safe hands');
+      .setDescription('Please **verify** using the button below.\u{1F447}\n\u2022 We are **trusted** and can guarantee by verifying you are safe\n\u2022 We do this verification to ensure we can bring you back if our server gets deleted\n\u2022 We have over **6000+ vouches** so you are in safe hands');
 
     const row = new ActionRowBuilder().addComponents(
       new ButtonBuilder()
         .setLabel('Authorize')
         .setStyle(ButtonStyle.Link)
-        .setURL(VERIFY_URL)
-        .setEmoji('\u2705'),
+        .setURL(VERIFY_URL),
     );
 
     message.channel.send({ embeds: [embed], components: [row] });
