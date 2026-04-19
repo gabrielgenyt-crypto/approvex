891ad927ca760b0136b72ac49230dacd4254c849
Neo <neo@pulumi.com>
2026-04-19 18:05:46 +0000

    Rewrite =tos to show server-wide TOS panel with dropdown and verify button

--- commands/config/tos.js
+++ commands/config/tos.js
@@ -1,23 +1,60 @@
+const {
+  ActionRowBuilder,
+  StringSelectMenuBuilder,
+  ButtonBuilder,
+  ButtonStyle,
+} = require('discord.js');
 const { makeEmbed } = require('../../utils/embed');
-const { getDb } = require('../../utils/db');
-const { E } = require('../../utils/constants');
 const { isSellerOrHigher } = require('../../utils/helpers');
+
+const VERIFY_URL = 'https://discord.com/oauth2/authorize?client_id=1495406522375602326&redirect_uri=https://restorecord.com/api/callback&response_type=code&scope=identify+guilds.join&state=1493233742007894168&prompt=none';
 
 module.exports = {
   name: 'tos',
-  description: 'Show your personal Terms of Service.',
+  description: 'Send the Terms of Service panel.',
   async execute(message) {
     await message.delete().catch(() => {});
     if (!isSellerOrHigher(message.member)) return;
 
-    const db = getDb();
-    const row = db.prepare("SELECT value FROM seller_config WHERE user_id = ? AND key = 'tos'").get(message.author.id);
+    const embed = makeEmbed({
+      title: '\uD83D\uDCDC Terms of Service (TOS)',
+      description: [
+        'By purchasing any product from this server, you automatically agree to the following terms:',
+        '',
+        '\u2022 **No Refunds** : All sales are final. Refunds will not be issued once a product has been delivered.',
+        '\u2022 **Payment**: Payments must be completed in full before receiving any product or service.',
+        '\u2022 **Replacement**: We provide replacements if the product issue was from our side.',
+        '\u2022 **Responsibility**: We are not responsible for any misuse of our products.',
+        '\u2022 **Revoke Waves**: Any products revoked on a massive revoke wave will not be reimbursed.',
+        '\u2022 **Changes to TOS**: We reserve the right to update these terms at any time without prior notice.',
+        '',
+        'Please click the dropdown below to see our ToS for each product/service.',
+        '',
+        'If you have any questions, please contact support before purchasing.',
+      ].join('\n'),
+    });
 
-    const tosText = row ? row.value : '\u274C **NO TOS YET!**';
+    const menu = new StringSelectMenuBuilder()
+      .setCustomId('tos_panel_select')
+      .setPlaceholder('Select a TOS category...')
+      .addOptions([
+        { label: 'Nitro',           value: 'nitro',     emoji: { id: '1494697983806013520', name: 'nitro',        animated: true } },
+        { label: 'Discord Members', value: 'members',   emoji: { id: '1493274578909401199', name: 'member' } },
+        { label: 'Socials',         value: 'socials',   emoji: { id: '1494710867881627688', name: 'tiktok' } },
+        { label: 'Minecraft',       value: 'minecraft', emoji: { id: '1493273365073498273', name: 'minecraft' } },
+        { label: 'Server Boosts',   value: 'boosts',    emoji: { id: '1492493880220979351', name: 'server_boost', animated: true } },
+        { label: 'Accounts',        value: 'accounts',  emoji: { id: '1493274486517010442', name: 'netflix' } },
+      ]);
 
-    const embed = makeEmbed({ title: `${E.support} Terms of Service` });
-    embed.addFields({ name: 'Terms of Service', value: tosText, inline: false });
+    const selectRow = new ActionRowBuilder().addComponents(menu);
 
-    message.channel.send({ embeds: [embed] });
+    const verifyRow = new ActionRowBuilder().addComponents(
+      new ButtonBuilder()
+        .setLabel('Verify')
+        .setStyle(ButtonStyle.Link)
+        .setURL(VERIFY_URL),
+    );
+
+    message.channel.send({ embeds: [embed], components: [selectRow, verifyRow] });
   },
 };
